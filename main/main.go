74542f6e8845099840f9489c73167466f0e99da5
ravindugm <ravindumiyuranga@gmail.com>
2022-02-17 21:03:37 +0530

    Initial Commit

---
+++ main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("--------------------------------------------------")
+	fmt.Println("|      Welcome to RMDev Tickets Booking App      |")
+	fmt.Println("--------------------------------------------------")
+
+}
