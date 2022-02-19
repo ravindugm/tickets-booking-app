6e6fb728cce5fd7ea3d29224ffe02dd8f6ea9773
ravindugm <ravindumiyuranga@gmail.com>
2022-02-19 23:38:51 +0530

    feature/Add users

---
+++ conf/user.go
@@ -0,0 +1,24 @@
+package conf
+
+import "fmt"
+
+func GetUserInputs() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"tickets-booking-app/conf"
+)
+
+const conferenceTickets int = 50
+
+var conferenceName string = "Go Conference"
+var remainingTickets uint = 50
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
 
 func main() {
+	greetUser(conferenceName, conferenceTickets, remainingTickets)
+
+	conf.GetUserInputs()
+
+}
+
+func greetUser(conferenceName string, conferenceTickets int, remainingTickets uint) {
 	fmt.Println("--------------------------------------------------")
 	fmt.Println("|      Welcome to RMDev Tickets Booking App      |")
 	fmt.Println("--------------------------------------------------")
-
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+	fmt.Println("**************************************************")
+	fmt.Println("**************************************************")
 }
