9ec491e8b3e88e3cd1d970980c1ca261667ab81d
ravindugm <ravindumiyuranga@gmail.com>
2022-02-20 00:03:29 +0530

    feature/Add user validation

--- main.go
+++ main.go
@@ -21,7 +21,31 @@ type UserData struct {
 func main() {
 	greetUser(conferenceName, conferenceTickets, remainingTickets)
 
-	conf.GetUserInputs()
+	for {
+		firstName, lastName, email, userTickets := conf.GetUserInputs()
+		isValidName, isValidEmail, isValidTicketNumber := conf.ValidUserInput(firstName, lastName, email, userTickets, remainingTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			bookTicket(userTickets, firstName, lastName, email)
+
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("Firstname or Lastname you entered is to short.")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain '@' sign.")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid.")
+			}
+		}
+
+	}
 
 }
 
@@ -34,4 +58,22 @@ func greetUser(conferenceName string, conferenceTickets int, remainingTickets ui
 	fmt.Println("Get your tickets here to attend")
 	fmt.Println("**************************************************")
 	fmt.Println("**************************************************")
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+
+	fmt.Printf("List of bookings: %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 }
